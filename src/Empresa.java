d40385d2a2281f0e6f2e8b73024cd07650ba0d70
Everton Oliveira <evertonhenrique.deoliveira@gmail.com>
2023-03-17 16:18:52 -0300

    primeiro comiti

---
+++ src/Empresa.java
@@ -0,0 +1,63 @@
+
+public class Empresa {
+	private Integer id;
+	public String nome;
+	private String cnpj;
+	private Double taxa;
+	private Double saldo;
+//  padronizando o tipo de dado para Double
+
+	public Empresa() {
+		super();
+	}
+
+	public Empresa(Integer id, String nome, String cnpj, Double taxa, Double saldo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cnpj = cnpj;
+		this.taxa = taxa;
+		this.saldo = saldo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public Double getTaxa() {
+		return taxa;
+	}
+
+	public void setTaxa(Double taxa) {
+		this.taxa = taxa;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+}
